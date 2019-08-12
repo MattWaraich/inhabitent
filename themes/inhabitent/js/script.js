b819b4bb6792624297ce2be23f6d74d1fb66e91c
Matt Waraich <mattsinghwaraich@gmail.com>
2019-08-12 15:35:39 -0700

    testing commit user email

--- themes/inhabitent/js/script.js
+++ themes/inhabitent/js/script.js
@@ -1,4 +1,5 @@
 (function() {
+  // TODO remove this line of code below
   $('body').append('hello world');
 })(jQuery);
 //IIFE: Imediately Invoked Function Expression b/c we can pass in global variable
